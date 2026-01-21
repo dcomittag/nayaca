a2e0170878b69876bf860685240dc17e93971a09
Daniel Mittag <dcomittag@gmail.com>
2026-01-21 20:01:42 +0700

    fddf

--- common.js
+++ common.js
@@ -1,10 +1,16 @@
 // Navbar shrink on scroll
-$(window).on('scroll', function() {
-  if ($(this).scrollTop() > 0) {
-    $('.navbar').addClass('navbar-scroll');
-    $('.navbar-brand img').css('height', '33px');
-  } else {
-    $('.navbar').removeClass('navbar-scroll');
-    $('.navbar-brand img').css('height', '42px');
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.navbar');
+  const logo = document.querySelector('.navbar-brand img');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scroll');
+      logo.style.height = '33px';
+    } else {
+      navbar.classList.remove('navbar-scroll');
+      logo.style.height = '42px';
+    }
+  });
 });
+
