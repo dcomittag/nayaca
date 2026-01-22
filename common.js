545afa239cd63f709ae5296d584413fc64727297
Daniel Mittag <dcomittag@gmail.com>
2026-01-22 16:36:27 +0700

    fg

--- common.js
+++ common.js
@@ -3,6 +3,13 @@ const toggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.nav-links');
 toggle.addEventListener('click', () => {
   links.classList.toggle('open');
+  // Add margin to the container so content moves down
+  const container = document.querySelector('.container');
+  if (links.classList.contains('open')) {
+    container.style.marginTop = links.offsetHeight + 'px';
+  } else {
+    container.style.marginTop = null; // reset
+  }
 });
 
 // Shrink navbar on scroll
